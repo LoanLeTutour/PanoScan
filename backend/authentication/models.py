62334cd579e641e5507fa8d01c69125d1fdaad99
Loan <letutourloan@gmail.com>
2024-06-24 17:49:29 +0200

    first migrations done - database created - few changes on user model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,17 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from panoscan.models import Market
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
+    username = None
+    
     name = models.CharField(max_length=100)
     postal_code = models.IntegerField()
     city = models.CharField(max_length=100)
     adress = models.CharField(max_length=500)
     country = models.CharField(max_length=30)
     price_per_month = models.IntegerField()
-    market = models.ForeignKey(Market, on_delete=models.SET_NULL)
+    market = models.ForeignKey(Market,null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.name
+
