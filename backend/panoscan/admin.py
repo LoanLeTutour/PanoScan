a8079a7efef52eb11aae907fc9953f9ebbc99629
Loan <letutourloan@gmail.com>
2024-06-26 19:40:47 +0200

    updates to be able to manage the database from the admin django - Les décors unis de chez SwissKrono en Suisse sont ajoutés à la db

--- backend/panoscan/admin.py
+++ backend/panoscan/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from panoscan.models import Market, Producer, PanelType, Structure, Collection, Decor, PanelTypesForStructureDecorCombination, StructuresForDecor, DecorsForCollection, PhotoTraining, PhotoUser
 
-# Register your models here.
+
+admin.site.register(Market)
+admin.site.register(Producer)
+admin.site.register(PanelType)
+admin.site.register(Structure)
+admin.site.register(Collection)
+admin.site.register(Decor)
+admin.site.register(PanelTypesForStructureDecorCombination)
+admin.site.register(StructuresForDecor)
+admin.site.register(DecorsForCollection)
+admin.site.register(PhotoTraining)
+admin.site.register(PhotoUser)
--- backend/panoscan/models.py
+++ backend/panoscan/models.py
@@ -2,34 +2,46 @@ from django.db import models
 from django.conf import settings
 class Market(models.Model):
     name = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.name
 
 class Producer(models.Model):
     name = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.name
 
 class PanelType(models.Model):
     name = models.CharField(max_length=100)
     length_in_mm = models.IntegerField()
     width_in_mm = models.IntegerField()
-    thickness_in_mm = models.IntegerField()
+    thickness_in_mm = models.FloatField()
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f'{self.name} - {self.producer} - {self.length_in_mm}x{self.width_in_mm}x{self.thickness_in_mm}' 
 
 class Structure(models.Model):
     name = models.CharField(max_length=30)
     code = models.CharField(max_length=10)
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f'{self.code}-{self.producer}'
 class Decor(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=10)
+    ncs_equivalent = models.CharField(max_length=15, null=True, blank=True)
     image = models.ImageField(verbose_name="Photo fabricant")
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
-    panel_type = models.ManyToManyField(PanelType, through='PanelTypesForDecor')
     structure = models.ManyToManyField(Structure, through='StructuresForDecor')
-    
+    def __str__(self) -> str:
+        return f'{self.code}-{self.producer}'
 class Collection(models.Model):
     name = models.CharField(max_length=30)
     market = models.ForeignKey(Market, on_delete=models.CASCADE)
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
-    decors = models.ManyToManyField(Decor, through='CollectionOfDecors')
+    decors = models.ManyToManyField(Decor, through='DecorsForCollection')
+    def __str__(self) -> str:
+        return f'{self.name}-{self.producer}-{self.market}'
 class PhotoTraining(models.Model):
     photo = models.ImageField()
     decor = models.ForeignKey(Decor, on_delete=models.CASCADE)
@@ -40,21 +52,26 @@ class PhotoUser(models.Model):
     date_tested = models.DateTimeField(auto_now_add=True)
     result = models.JSONField()
 
-class CollectionOfDecors(models.Model):
+class DecorsForCollection(models.Model):
     decor = models.ForeignKey(Decor, on_delete=models.CASCADE)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     class Meta:
         unique_together = ('decor', 'collection')
 
 
-class PanelTypesForDecor(models.Model):
-    decor = models.ForeignKey(Decor, on_delete=models.CASCADE)
-    panel_type = models.ForeignKey(PanelType, on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('decor', 'panel_type')
-
 class StructuresForDecor(models.Model):
     decor = models.ForeignKey(Decor, on_delete=models.CASCADE)
     structure = models.ForeignKey(Structure, on_delete=models.CASCADE)
+    panel_type = models.ManyToManyField(PanelType, through='PanelTypesForStructureDecorCombination')
+    
+    def __str__(self) -> str:
+        return f'{self.decor.code} {self.structure.code}'
     class Meta:
         unique_together = ('decor', 'structure')
+    
+
+class PanelTypesForStructureDecorCombination(models.Model):
+    decor_structure_combination = models.ForeignKey(StructuresForDecor, on_delete=models.CASCADE, default=1)
+    panel_type = models.ForeignKey(PanelType, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('decor_structure_combination', 'panel_type')
