18d647eb670e57fd6680deb464671763259e0a1b
Loan <letutourloan@gmail.com>
2024-11-07 17:50:38 +0100

    Backend: Modification de la méthode POST des PhotosUser et ajout du modèle Argolite_first_12

--- backend/panoscan/views.py
+++ backend/panoscan/views.py
@@ -14,7 +14,7 @@ from .serializers import StructureListSerializer, StructureDetailSerializer
 from .serializers import CollectionListSerializer, CollectionDetailSerializer
 from .serializers import DecorListSerializer, DecorDetailSerializer
 from .serializers import FinalProductSerializer
-from .serializers import StructuresForDecorSerializer
+from .serializers import StructuresForDecorDetailSerializer, StructuresForDecorListSerializer
 from .serializers import DecorsForCollectionListSerializer, DecorsForCollectionDetailSerializer
 from .serializers import PhotoUserSerializer
 
@@ -199,7 +199,7 @@ class AdminDecorsForCollectionViewset(MultipleSerializerMixin, ModelViewSet):
     permission_classes = [IsAdminAuthenticated, IsStaffAuthenticated]
     queryset = DecorsForCollection.objects.all()
 class StructuresForDecorViewset(ReadOnlyModelViewSet):
-    serializer_class = StructuresForDecorSerializer
+    serializer_class = StructuresForDecorDetailSerializer
     def get_queryset(self):
         queryset = StructuresForDecor.objects.filter(active=True)
         decor_id = self.request.GET.get('decor_id')
@@ -215,7 +215,7 @@ class StructuresForDecorViewset(ReadOnlyModelViewSet):
         return Response()
 
 class AdminStructuresForDecorViewset(ModelViewSet):
-    serializer_class = StructuresForDecorSerializer
+    serializer_class = StructuresForDecorDetailSerializer
     permission_classes = [IsAdminAuthenticated, IsStaffAuthenticated]
     queryset = StructuresForDecor.objects.all()
     
@@ -230,7 +230,6 @@ class AdminFinalProductViewset(ModelViewSet):
     queryset = FinalProduct.objects.all()
 
 
-## Ajout de photos pour l'entrainement du modèle
 import tempfile
 from .management.utils.save_photo_user_drive import save_photo_user_drive
 class PhotoUploadView(APIView):
@@ -238,7 +237,8 @@ class PhotoUploadView(APIView):
 
     def post(self, request, *args, **kwargs):
         print("Headers: ", request.headers)
-        data = {key: value for key, value in request.data.items() if key != 'photo'}
+        data = {key: value for key, value in request.data.items() if key != 'photo' }
+        market_id = data['market_id']
         data['user'] = request.user.id
 
         if 'photo' not in request.FILES:
@@ -259,20 +259,145 @@ class PhotoUploadView(APIView):
                 return Response({'error': 'Failed to upload file to Google Drive'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         data['photo_url'] = photo_url
+        # Rechercher le modèle : 
+        import torch
+        import torch.nn as nn
+        from torchvision.transforms import v2
+        from PIL import Image
+
+        # Enregistrer temporairement l'image sur le disque pour l'inférence
+        with tempfile.NamedTemporaryFile(delete=False) as tmp:
+            for chunk in photo.chunks():
+                tmp.write(chunk)
+            tmp_path = tmp.name
+
+
+        test_transform = v2.Compose([
+            v2.Resize(size=(64,64)),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+        ])
+
+
+# Appliquer les transformations
+        def preprocess_image(image_path):
+            print(f"Preprocessing the photo...")
+            img = Image.open(image_path).convert('RGB')
+            img_transformed = test_transform(img)
+    # Ajouter une dimension batch (car le modèle s'attend à une entrée de forme [batch_size, channels, height, width])
+            img_transformed = img_transformed.unsqueeze(0)
+            return img_transformed
+        class Modèle_Argolite_12(nn.Module):
+            def __init__(self, input_shape: int = 3,
+               hidden_units: int = 64,
+               output_shape: int = 12) -> None:
+                super().__init__()
+                self.conv_block_1 = nn.Sequential(
+                    nn.Conv2d(in_channels=input_shape,
+                  out_channels=hidden_units,
+                  kernel_size=6,
+                  stride=1,
+                  padding=1),
+                    nn.BatchNorm2d(hidden_units),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=hidden_units,
+                  out_channels=hidden_units*2,
+                  kernel_size=6,
+                  stride=1,
+                  padding=1),
+                    nn.BatchNorm2d(hidden_units*2),
+                    nn.ReLU(),
+                    nn.MaxPool2d(kernel_size=2,
+                     stride=2)
+                )
+                self.conv_block_2 = nn.Sequential(
+                    nn.Conv2d(in_channels=hidden_units*2,
+                  out_channels=hidden_units*4,
+                  kernel_size=6,
+                  stride=1,
+                  padding=1),
+                    nn.BatchNorm2d(hidden_units*4),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=hidden_units*4,
+                  out_channels=hidden_units*4,
+                  kernel_size=6,
+                  stride=1,
+                  padding=1),
+                    nn.BatchNorm2d(hidden_units*4),
+                    nn.ReLU(),
+                    nn.MaxPool2d(kernel_size=2,
+                     stride=2)
+                )
+
+                self.classifier = nn.Sequential(
+                    nn.Flatten(),
+                    nn.Linear(in_features=hidden_units*11*11*4,out_features=output_shape),
+                    )
+
+            def forward(self, x):
+                return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+            
+        def load_model(model_path):
+                print(f"loading the model...")
+            # Définir ou charger le modèle (ajuste cette partie avec ton propre modèle)
+                model = Modèle_Argolite_12() 
+                model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+                model.eval()  # Mettre le modèle en mode évaluation
+                return model
+            
+        def predict(model, image_tensor):
+                print(f"predicting")
+                list_of_decors = ["216", "217", "246", "276", "281", "301", "302", "306", "330", "352", "377", "394"]
+                with torch.inference_mode(): 
+                    output = model(image_tensor)  # Passer l'image à travers le modèle
+                    _, predicted_class = torch.max(output, 1)  # Obtenir la classe prédite
+                return list_of_decors[predicted_class.item()]
+        producer_name = "Argolite"
+        producer_id = Producer.objects.get(name=producer_name).id
+        argolite_model_path = "/Users/maelissbuaud/Loan/panoscan/backend/panoscan/management/utils/models/argolite_first_12_model_0.pth"
+        argolite_dict = {'producer_name': producer_name, 'producer_id': producer_id, 'path': argolite_model_path}
+        model_dicts = [argolite_dict]
+        import os
+        # preprocessing ...
+        image_tensor = preprocess_image(tmp_path)
+        os.remove(tmp_path)
+        list_of_results = []
+        for model_dict in model_dicts:
+            try: 
+                model = load_model(model_dict['path']) #Loading the model
+                predicted_class = predict(model, image_tensor) #Predicting the class : output = 216 ou K 101
+                decorObject = Decor.objects.get(code=predicted_class, active=True) # On récupère l'objet décor de la bdd correspondant à la prédiction
+                print(decorObject)
+                collections = Collection.objects.filter(market__id=market_id, active=True) # On récupère les différentes collections de décors présentes sur le marché du client
+                print(collections)
+                decor_collection_object = DecorsForCollection.objects.get(decor=decorObject, collection__in=collections) # On récupère l'objet DecorCollection correspondant à la prédiction
+                print(decor_collection_object)
+                result = {'producer_name': model_dict['producer_name'],
+                          'producer_id': model_dict['producer_id'],
+                            'decor_collection_id': decor_collection_object.id,
+                            'photo_url': decor_collection_object.decor.photo_url, 
+                            'collection_name': decor_collection_object.collection.name, 
+                            'decor_code': decor_collection_object.decor.code,
+                            'decor_name': decor_collection_object.decor.name}
+                print(result)
+                list_of_results.append(result)
+                print(list_of_results)
+            except Exception as e:
+                print(f"Erreur lors de la prédiction : {e}")
+                return Response({'error': f'Prediction failed on model {model_dict['path']}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         serializer = PhotoUserSerializer(data=data)
         if serializer.is_valid():
             serializer.save(user=request.user)
+            serializer.save(result=list_of_results)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .models import PhotoUser
 from .serializers import PhotoUserSerializer
-
 
 class PhotoUserViewSet(ModelViewSet):
     @action(detail=True, methods=['get'])
